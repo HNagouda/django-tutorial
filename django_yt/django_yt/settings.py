0025ab05d3383d7f3a9eba334b196877b5beca0a
Harsh Nagouda <harshpnagouda@gmail.com>
2021-12-25 14:02:48 -0500

    finished 6th vid

--- django_yt/django_yt/settings.py
+++ django_yt/django_yt/settings.py
@@ -31,7 +31,11 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Created Apps
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms', 
+    # Defaults
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
